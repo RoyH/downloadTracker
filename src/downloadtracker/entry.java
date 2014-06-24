7cdf765c57d2a44cdda5d9a46888cf64e131c03e
Roy <sukrith@gmail.com>
2014-06-24 19:09:43 +0100

    Look up table function implemented

--- src/downloadtracker/entry.java
+++ src/downloadtracker/entry.java
@@ -4,38 +4,58 @@
  */
 package downloadtracker;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Roy
  */
 public class entry {
     private String Show;
-    private String Episode;
+    private int Episode;
     private String ipAddress;
     private String Date; 
-    
+    private String EpisodeName;
     // Constructor
     
     public entry(String show, String Episode, String ipAddress, String Date) {
         this.Show = show;
-        this.Episode = Episode;
+        try {
+            this.Episode =  Integer.parseInt(Episode);
+        } catch (Exception e) {
+        }
+        
         this.ipAddress = ipAddress;
         this.Date = Date;
+        this.EpisodeName = "";
 
     } 
     
     public void output() {
-        System.out.println(ipAddress + " " + Show + " " + Episode + " " + Date);
+        System.out.println(ipAddress + " " + Show + " " + Episode + " " + Date + " " + EpisodeName);
         
     }
     
     public String getString() {
-        return (ipAddress + "-" + Show + "-" + Episode + "-" + Date);
+        return (ipAddress + "-" + Show + "-" + Episode + "-" + Date + "-" + EpisodeName);
     }
    
     public void checkData() {
         if (Show.equals("")) {
             System.out.println("error");
+        }
+    }
+    
+    public void resolveName(String[][] lookup) {
+        //need to change back to COMPARE STRING!!!
+        for (int i = 0; i < lookup[0].length; i++) {
+           if((Show.equals(lookup[0][i])) && (Episode == Integer.parseInt(lookup[1][i]))) {
+               EpisodeName = lookup[2][i];
+               break;
+           } else {
+               EpisodeName = "Unknown";
+           }
+           
         }
     }
     
